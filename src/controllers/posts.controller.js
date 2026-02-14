749514eb136644b08a7b70a11a385ffe02afee21
benj-bernardi <benjaminbernardi33@gmail.com>
2026-02-13 23:44:44 -0300

    feat: implement post creation with validation and user association

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,46 @@
+import pool from "../database/db.js";
+
+export async function createPost(req, res, next) {
+    try {
+        const { title, content } = req.body;
+        const userId = req.user.id;
+
+        if (!title || !content){
+            return res.status(400).json({ error: "Title and content are required" });
+        }
+
+        if (title.trim().length < 3 || title.trim().length > 120){
+            return res.status(400).json({ error: "Title must be between 3 and 120 characters"});
+        }
+
+        if (content.trim().length < 5) {
+            return res.status(400).json({ error: "Content must be at least 5 characters long" });
+        }
+
+        const userSelection = await pool.query("SELECT name FROM users WHERE id = $1", [userId]);
+
+        if (userSelection.rows.length < 0){
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const user = userSelection.rows[0];
+
+        const result = await pool.query(
+            "INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3) RETURNING title, content, user_id, created_at",
+            [title, content, userId]);
+        
+        const newPost = result.rows[0]; 
+
+        res.status(201).json({
+            message: "Post created successfuly", 
+            post: { 
+                userId: userId,
+                name: user.name,
+                title: newPost.title,
+                content: newPost.content
+            }
+        });
+    } catch (err){
+        next(err);
+    }
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { registerUser, loginUser, getMe } from "../controllers/user.controller.js";
 import { authMiddleware } from  "../middlewares/authMiddleware.js";
+import { createPost } from "../controllers/posts.controller.js";
 
 const router = Router();
 
 router.get("/me", authMiddleware, getMe);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/posts", authMiddleware, createPost);
 
 export default router;
