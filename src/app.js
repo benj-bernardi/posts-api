fc08c26051aa2c4c044fc3f56d950c6245ba83ed
benj-bernardi <benjaminbernardi33@gmail.com>
2026-04-12 18:47:42 -0300

    refactor(routes): separate posts and comments routes and fix route conflicts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import userRotes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js"
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", userRotes);
+app.use("/admin", adminRoutes);
 app.use(errorHandler);
 
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,29 @@
 import { Router } from "express";
 import { registerUser, loginUser, getMe, updateUser } from "../controllers/user.controller.js";
-import { getPosts, createPost, updatePost, deletePost,  } from "../controllers/posts.controller.js";
+import { getPosts, createPost, updatePost, deletePost } from "../controllers/posts.controller.js";
 import { createComment, deleteComment } from "../controllers/comments.controller.js";
-import { authMiddleware } from  "../middlewares/authMiddleware.js";
-
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { refreshToken } from "../controllers/refresh.controller.js";
 
 const router = Router();
 
-// Auth Routes
+// auth
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh", refreshToken);
 
-// User Routes
+// user
 router.get("/me", authMiddleware, getMe);
 router.patch("/me", authMiddleware, updateUser);
 
-// Posts Routes
-router.get("/posts", authMiddleware, getPosts )
+// posts
+router.get("/posts", authMiddleware, getPosts);
 router.post("/posts", authMiddleware, createPost);
-router.post("/posts", authMiddleware, createComment);
 router.patch("/posts/:id", authMiddleware, updatePost);
 router.delete("/posts/:id", authMiddleware, deletePost);
-router.delete("/posts/:id", authMiddleware, deleteComment);
+
+// comments
+router.post("/posts/:id/comments", authMiddleware, createComment);
+router.delete("/comments/:id", authMiddleware, deleteComment);
 
 export default router;
