abcfb0f2a6c3bcc86e1ca2218d19a159d6345833
benj-bernardi <benjaminbernardi33@gmail.com>
2026-06-07 00:01:09 -0300

    feat(auth): add refresh token route

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,1 +1,11 @@
-//... 
+import { rateLimit } from "express-rate-limit";
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 10,
+  message: {
+    error: "Too many login attempts, try again later."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
